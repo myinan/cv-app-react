1099df1016bbf3877b1fd056fcef9e3777a671eb
myinan <m.yasir.inan@gmail.com>
2024-03-07 03:21:45 +0300

    Fix personInfo descriptions

--- src/components/personInfo.jsx
+++ src/components/personInfo.jsx
@@ -28,8 +28,7 @@ const originalPersonInfoData = {
       startDate: "08/2020",
       endDate: "present",
       location: "New York City, US",
-      description: `Designed and prototyped user interface patterns for various clients in various industries,
-         ranging from self-service apps within the telecommunications-sector to mobile games for IOS and Android.`,
+      description: `Designed and prototyped user interface patterns for various clients in various industries, ranging from self-service apps within the telecommunications-sector to mobile games for IOS and Android.`,
     },
 
     {
@@ -38,9 +37,7 @@ const originalPersonInfoData = {
       startDate: "04/2018",
       endDate: "02/2019",
       location: "Berlin, Germany",
-      description: `Supported senior researchers on accessibility standards for the open web. 
-      Created and usability tested wireframes and prototypes. 
-      Produced interactive documentation for quick onboarding of new researchers.`,
+      description: `Supported senior researchers on accessibility standards for the open web. Created and usability tested wireframes and prototypes. Produced interactive documentation for quick onboarding of new researchers.`,
     },
   ],
 };
@@ -77,8 +74,7 @@ const personInfoData = {
       startDate: "08/2020",
       endDate: "present",
       location: "New York City, US",
-      description: `Designed and prototyped user interface patterns for various clients in various industries,
-         ranging from self-service apps within the telecommunications-sector to mobile games for IOS and Android.`,
+      description: `Designed and prototyped user interface patterns for various clients in various industries, ranging from self-service apps within the telecommunications-sector to mobile games for IOS and Android.`,
     },
 
     {
@@ -87,9 +83,7 @@ const personInfoData = {
       startDate: "04/2018",
       endDate: "02/2019",
       location: "Berlin, Germany",
-      description: `Supported senior researchers on accessibility standards for the open web. 
-      Created and usability tested wireframes and prototypes. 
-      Produced interactive documentation for quick onboarding of new researchers.`,
+      description: `Supported senior researchers on accessibility standards for the open web. Created and usability tested wireframes and prototypes. Produced interactive documentation for quick onboarding of new researchers.`,
     },
   ],
 };
